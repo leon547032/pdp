a72cca8dab2854db8d5a199ccffcf364ce8dcd3f
leon547032 <leon547032@gmail.com>
2025-03-26 12:38:21 +0900

    Update conver.java

--- conver.java
+++ conver.java
@@ -2,14 +2,24 @@ List<SomeDTO> dtoList = someEntityList.stream()
     .map(entity -> {
         SomeDTO dto = BeanCopyUtils.copy(entity, SomeDTO.class);
 
-        // SubEntity → SubDTO 변환
-        List<SubDTO> subDtoList = Optional.ofNullable(entity.getSubEntities())
+        // SubEntity -> SubDTO
+        List<SubDTO> subDTOs = Optional.ofNullable(entity.getSubEntities())
             .orElse(Collections.emptyList())
             .stream()
-            .map(sub -> BeanCopyUtils.copy(sub, SubDTO.class))
+            .map(subEntity -> {
+                SubDTO subDTO = BeanCopyUtils.copy(subEntity, SubDTO.class);
+
+                // IdEntity -> IdDTO
+                if (subEntity.getIdEntity() != null) {
+                    IdDTO idDTO = BeanCopyUtils.copy(subEntity.getIdEntity(), IdDTO.class);
+                    subDTO.setIdDTO(idDTO);
+                }
+
+                return subDTO;
+            })
             .collect(Collectors.toList());
 
-        dto.setSubList(subDtoList);
+        dto.setSubList(subDTOs);
         return dto;
     })
     .collect(Collectors.toList());
